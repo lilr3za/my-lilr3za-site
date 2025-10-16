e856e75628c0ba68ba8ccb74dc07a29c63f46404
lilr3za <98772312+lilr3za@users.noreply.github.com>
2025-10-16 13:49:57 +0330

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -2,12 +2,9 @@
 
 const { factory, vercel } = require('decap-cms-oauth-provider-vercel');
 
-// This creates the handler function with the correct settings
-const handler = factory({
-  // The client ID and secret are read from the environment variables you set in Vercel
-  clientId: process.env.OAUTH_CLIENT_ID,
-  clientSecret: process.env.OAUTH_CLIENT_SECRET,
-});
-
-// This exports the handler in the format that Vercel expects
-module.exports = vercel(handler);
+module.exports = vercel(
+  factory({
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  })
+);
